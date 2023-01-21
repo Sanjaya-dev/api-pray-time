6a659d723ee4f8f8b51118de651b8773a12bdd32
Fernanda <sanjaya.dev20@gmail.com>
2023-01-21 17:08:14 +0700

    menampilkan data api pada table

--- app.js
+++ app.js
@@ -2,10 +2,24 @@ function prayerTime(latitude,longitude){
     fetch('http://api.aladhan.com/v1/calendar?latitude='+latitude+'&longitude='+longitude+'&method=2')
     .then(response => response.json())
     .then(function(response){
-        let date = new Date();
-        let today = date.getDate() - 1;
-    
-        console.log(response.data[today]);
+        let date    = new Date();
+        let today   = date.getDate() - 1;
+        let data    = response.data[today].timings;
+        console.log(response.data);
+        let app             = document.getElementById('app');
+        let table           = document.createElement('table');
+        let tableTbody      = document.createElement('tbody');
+        
+        for(i in data){
+            let row         = tableTbody.insertRow();
+            let name        = row.insertCell(0);
+            let time        = row.insertCell(1);
+            name.innerHTML  = i;
+            time.innerHTML  = data[i];
+            tableTbody.appendChild(row);
+        }
+        table.appendChild(tableTbody)   
+        app.appendChild(table);
     });
 }
 
